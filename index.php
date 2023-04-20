9d70c2806dbf68afac8b1151205f9cdc550d501d
phildub <phil@potager.tech>
2023-04-20 14:11:38 -0400

    goooo

--- index.php
+++ index.php
@@ -2,6 +2,19 @@
 $ddate = "2023-04-20";
 $date = new DateTime($ddate);
 $week = $date->format("W");
-echo "Weeknummer: $week";
+echo "Week number: $week";
+
+if ($week % 2 == 0)
+{ $string = 'Yes, it is recycling week in Canton.'; }
+else
+{ $string = 'Nope. Not this week.'; }
 
 ?>
+<html>
+<head>
+	<title>Canton Recycling Day - Baltimore City</title>
+</head>
+<body>
+	<h1><?php echo $string; ?></h1>
+</body>
+</html>
